54ae7238fcb4c4816f4faffe220338ad1bd4bb1f
Sergio Diniz <sergiodinizsh@gmail.com>
2017-07-11 13:41:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Ola} from './node_modules'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <Ola />
+      </div>
+    );
+  }
+}
+
+export default App;
